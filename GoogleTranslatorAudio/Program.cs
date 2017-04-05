1e4c0bbc704337fec3d304fab2fe87cf90946279
bastyuchenko <bastyuchenko@gmail.com>
2017-04-05 09:47:51 +0300

    Add project files.

---
+++ GoogleTranslatorAudio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GoogleTranslatorAudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
